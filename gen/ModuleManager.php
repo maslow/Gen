0ee80c3cebe5f3d0b97136245d9325e0c74dff7f
maslow <wangfugen@126.com>
2016-05-06 20:13:27 +0800

    Implement ModuleManager more.

--- gen/ModuleManager.php
+++ gen/ModuleManager.php
@@ -9,6 +9,8 @@
 namespace app\gen;
 
 use yii\base\Exception;
+use yii\helpers\ArrayHelper;
+use yii\helpers\FileHelper;
 
 /**
  * Class ModuleManager
@@ -63,13 +65,13 @@ class ModuleManager
         return 'app\\module';
     }
 
-
     /**
+     * @param $module_id string
      * @return string
      */
-    public static function getModuleExternalFileName()
+    public static function getModuleFullClassName($module_id)
     {
-        return "external.php";
+        return self::getModuleNamespacePrefix() . '\\' . $module_id . '\\' . self::getModuleDefaultClassName();
     }
 
     /**
@@ -79,7 +81,26 @@ class ModuleManager
      */
     public static function getModuleExternalFilePath($module_id, $returnAlias = true)
     {
-        return self::getModuleRootPath($module_id, $returnAlias) . DIRECTORY_SEPARATOR . self::getModuleExternalFileName();
+        return self::getModuleRootPath($module_id, $returnAlias) . DIRECTORY_SEPARATOR . 'external.php';
+    }
+
+    /**
+     * @param $modules_id
+     * @param bool|true $returnAlias
+     * @return string
+     */
+    public static function getModuleMigrationsRootPath($modules_id, $returnAlias = true)
+    {
+        return self::getModuleRootPath($modules_id, $returnAlias) . DIRECTORY_SEPARATOR .'migrations';
+    }
+
+    /**
+     * @param $module_id
+     * @return bool
+     */
+    public static function hasMigrationFiles($module_id){
+        $files = FileHelper::findFiles(self::getModuleMigrationsRootPath($module_id,false));
+        return count($files) > 0;
     }
 
     /**
@@ -107,6 +128,33 @@ class ModuleManager
             return new ModuleInfo(self::loadModuleExternalFileContent($module_id));
         }
         return null;
+    }
+
+
+    /**
+     * @return array
+     */
+    public static function getModuleList()
+    {
+        $list = self::listDir(self::getModulesRootPath(false));
+        return $list;
+    }
+
+    /**
+     * @param $directory string
+     * @return array
+     */
+    protected static function listDir($directory)
+    {
+        $dirs = array();
+        $dir = dir($directory);
+        while ($file = $dir->read()) {
+            if ((is_dir("$directory/$file")) AND ($file != ".") AND ($file != "..")) {
+                array_push($dirs, $file);
+            }
+        }
+        $dir->close();
+        return $dirs;
     }
 
 
