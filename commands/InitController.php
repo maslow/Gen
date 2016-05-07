567d4f2f2787027a7b486419697734a0a1d19bde
maslow <wangfugen@126.com>
2016-05-07 15:55:24 +0800

    move commands to gen/commands

--- commands/InitController.php
+++ commands/InitController.php
@@ -1,47 +1,16 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * Author: Maslow(wangfugen@126.com)
+ * Date: 5/7/16
+ * Time: 3:49 PM
+ */
 
 namespace app\commands;
 
-use yii\console\Controller;
+use app\gen\commands\InitBaseController;
 
-/**
- * Class InitController , provides installation commands
- * @package app\commands
- */
-class InitController extends Controller
+class InitController extends InitBaseController
 {
-    /**
-     * Installation Commands
-     * Usages:
-     * ```
-     * php yii init
-     *
-     * #Equal to
-     * php yii init dev
-     *
-     * #Or for production environment
-     * php yii init prod
-     *
-     * ```
-     * @param string $env 'dev' for development environment or 'prod' for production environment
-     * @return bool|int
-     */
-    public function actionIndex($env = 'dev')
-    {
-        if ($env !== 'dev' && $env !== 'prod') return $this->stderr("ERROR: {$env} is invalid.");
 
-        $tplIndexPath = \Yii::getAlias("@app/environments/index-{$env}.php");
-        $tplDBPath = \Yii::getAlias("@app/environments/db-{$env}.php");
-
-        $targetDBPath = \Yii::getAlias('@app/config/db.php');
-        $targetIndexPath = \Yii::getAlias('@app/web/index.php');
-
-        file_exists($targetIndexPath) ? unlink($targetIndexPath) : null;
-
-        if (!file_exists($targetDBPath) && !file_put_contents($targetDBPath, file_get_contents($tplDBPath)))
-            return $this->stderr("ERROR: Generating {$targetDBPath} failed.");
-
-        if(!file_put_contents($targetIndexPath, file_get_contents($tplIndexPath)))
-            return $this->stderr("ERROR: Generating {$targetIndexPath} failed.");
-    }
 }
---
+++ gen/commands/InitBaseController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\gen\commands;
+
+use yii\console\Controller;
+
+/**
+ * Class InitBaseController , provides installation commands
+ * @package app\gen\commands
+ */
+class InitBaseController extends Controller
+{
+    /**
+     * Installation Commands
+     * Usages:
+     * ```
+     * php yii init
+     *
+     * #Equal to
+     * php yii init dev
+     *
+     * #Or for production environment
+     * php yii init prod
+     *
+     * ```
+     * @param string $env 'dev' for development environment or 'prod' for production environment
+     * @return bool|int
+     */
+    public function actionIndex($env = 'dev')
+    {
+        if ($env !== 'dev' && $env !== 'prod') return $this->stderr("ERROR: {$env} is invalid.");
+
+        $tplIndexPath = \Yii::getAlias("@app/environments/index-{$env}.php");
+        $tplDBPath = \Yii::getAlias("@app/environments/db-{$env}.php");
+
+        $targetDBPath = \Yii::getAlias('@app/config/db.php');
+        $targetIndexPath = \Yii::getAlias('@app/web/index.php');
+
+        file_exists($targetIndexPath) ? unlink($targetIndexPath) : null;
+
+        if (!file_exists($targetDBPath) && !file_put_contents($targetDBPath, file_get_contents($tplDBPath)))
+            return $this->stderr("ERROR: Generating {$targetDBPath} failed.");
+
+        if(!file_put_contents($targetIndexPath, file_get_contents($tplIndexPath)))
+            return $this->stderr("ERROR: Generating {$targetIndexPath} failed.");
+    }
+}
